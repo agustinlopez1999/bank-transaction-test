4c0b080f1e90fdb6a296d67e211c2eaa0145c74f
agustinlopez1999 <lopezagustin1999@hotmail.com>
2023-10-25 11:01:33 -0300

    first Git

---
+++ Bank.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.0;
+
+contract bankAccount{
+    address owner;
+
+    constructor(){
+        owner = msg.sender;
+    }
+
+    mapping (address => uint) balance;
+
+    function getBalance() public view returns(uint){
+        return balance[msg.sender];
+    }
+
+    function deposit(uint _amount) public{
+        balance[msg.sender] += _amount;
+    }
+
+    function withdraw(uint _amount) public{
+        require(balance[msg.sender] >= _amount, "Insufficient Balance");
+        balance[msg.sender] -= _amount;
+    }
+
+    function transfer(uint _amount, address _destination) public{
+        require(_destination != msg.sender, "You cannot transfer to yourself!");
+        require(balance[msg.sender] >= _amount, "Insufficient Balance");
+        balance[msg.sender] -= _amount;
+        balance[_destination] += _amount;
+    }
+
+}
